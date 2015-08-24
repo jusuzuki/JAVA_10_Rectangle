160c406e865b2ac898c0b83c0c029779563ec107
Juliana <jusuzuki@Julianas-MBP.hsd1.or.comcast.net>
2015-08-23 21:06:10 -0700

    Add initial files

---
+++ src/test/java/AppUnitTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class AppUnitTest {
+
+  @Test
+  public void methodName_testDescription_expectedOutcome() {
+    //assertEquals(true, App.methodName("Example Use Case"));
+  }
+}
